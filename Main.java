2385a18b0e6e8cdc5249702d3bbc5331e8c43848
NadRetsel <97971565+NadRetsel@users.noreply.github.com>
2024-02-20 12:28:03 +0000

    Very high level outline of solution

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TODO - Generate possible puzzle
+
+        // TODO - Mark each Cell with possible numbers
+            // TODO - Check with separate thread for row/column/box (?)
+
+
+        // TODO - Check for any Cells with guaranteed number
+            // TODO - If only 1 possible number for Cell, populate Cell and update row/column/box
+            // TODO - If only 1 number appears in row/column/box, populate cell and update
+
+
+        // TODO - Once every cell is occupied, find cell with duplicate possible numbers
+            // TODO - Pick cell (random or cell with fewest possibilities) and try on different threads
+            // TODO - If dead-end found, kill thread
+    }
+}
